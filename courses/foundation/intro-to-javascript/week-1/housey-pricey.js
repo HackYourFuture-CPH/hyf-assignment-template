440912b347f95d7e83ece7c2b95a209f61bb502e
Anastasia Solodova <132252079+nastiasolo@users.noreply.github.com>
2025-11-04 11:02:12 +0100

    js-week1: smart-ease

---
+++ courses/foundation/intro-to-javascript/week-1/housey-pricey.js
@@ -0,0 +1,21 @@
+let peterHouseWidth = 8;
+let peterHouseDepth = 10;
+let peterHouseHeight = 10;
+
+let peterGardenSizeInM2 = 100;
+let peterHouseVolume = peterHouseWidth * peterHouseDepth * peterHouseHeight;
+
+let peterHousePrice = peterHouseVolume * 2.5 * 1000 + peterGardenSizeInM2 * 300;
+console.log(peterHousePrice);
+//Peters house is expensive
+
+let juliaHouseWidth = 5;
+let juliaHouseDepth = 11;
+let juliaHouseHeight = 10;
+
+let juliaGardenSizeInM2 = 70;
+let juliaHouseVolume = juliaHouseWidth * juliaHouseDepth * juliaHouseHeight;
+
+let juliaHousePrice = juliaHouseVolume * 2.5 * 1000 + juliaGardenSizeInM2 * 300;
+console.log(juliaHousePrice);
+//Julias house is cheap
