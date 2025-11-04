3b827f1e78dcf48af415ca287fdcc316e7c43b8e
hiwot <99hiwot@gmail.com>
2025-11-04 20:40:10 +0100

    Future Age Calculator

---
+++ js-assignment-week1/scripts.js
@@ -0,0 +1,8 @@
+console.log("Hello HYF");
+let yearFuture = 2027;
+let age = 40;
+
+// Calculate year of birth based on future year and age
+let yearOfBirth = yearFuture - age;
+
+console.log("You will be " + age + " years old in " + yearFuture);
