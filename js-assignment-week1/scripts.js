566080ad07bcb513b5288d2721e8aa6999ee7ca2
hiwot <99hiwot@gmail.com>
2025-11-04 20:41:48 +0100

    Dog Age Calculator

--- js-assignment-week1/scripts.js
+++ js-assignment-week1/scripts.js
@@ -6,3 +6,16 @@ let age = 40;
 let yearOfBirth = yearFuture - age;
 
 console.log("You will be " + age + " years old in " + yearFuture);
+// dog age calculator
+let dogYearOfBirth;
+let dogYearFuture;
+let dogYear;
+let shouldShowResultInDogYears=true;
+//calculate dog year of birth
+ dogAge=10;
+dogYearOfBirth=dogYearFuture-dogAge;
+dogYearFuture=2027;
+if(shouldShowResultInDogYears){
+    dogYear=dogAge*7;
+    console.log("Your dog will be " + dogYear + " dog years old in" + dogYearFuture);
+}
