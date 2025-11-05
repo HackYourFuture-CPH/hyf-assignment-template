cf57d2d99e8e90277960e343bc08fcc8b6b39826
ZahraSH67 <zahra.shahabi.dk@gmail.com>
2025-11-05 11:12:58 +0100

    Added dogAge

--- smart-ease.io/script.js
+++ smart-ease.io/script.js
@@ -2,3 +2,19 @@ const yearOfBirth  = 1988; //Number
 const  yearFuture = 2027; //Number
 const age = yearFuture - yearOfBirth
 console.log(`You wll be ${age} years old in 2027.`)
+
+
+
+
+const dogYearOfBirth = 2018;
+const dogYearFuture = 2027;
+ const dogYear = dogYearFuture - dogYearOfBirth;
+const dogAgeInDogYears = dogYear * 7; 
+let shouldShowResultInDogYears  //Boolean
+if(shouldShowResultInDogYears == true){
+ dogAgeInDogYears 
+    console.log(`Your dog will be ${dogAgeInDogYears} years old in 2027`)
+}else{
+    dogYear 
+    console.log(`Your dog will be${dogYear} human years old in 2027`)
+}
