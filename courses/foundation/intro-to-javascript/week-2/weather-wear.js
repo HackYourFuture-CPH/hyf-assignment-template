a3cc99245f0852247173f248b9999ab6019d87e6
Anastasia Solodova <132252079+nastiasolo@users.noreply.github.com>
2025-11-11 15:45:40 +0100

    js-week2: fixed weather app (degrees)

--- courses/foundation/intro-to-javascript/week-2/weather-wear.js
+++ courses/foundation/intro-to-javascript/week-2/weather-wear.js
@@ -1,7 +1,7 @@
 function whatToWear(degrees) {
-  if (degrees < 0) {
+  if (degrees <= 0) {
     return "Wear a fur coat and don't forget a hat";
-  } else if (degrees > 0 && degrees < 10) {
+  } else if (degrees > 0 && degrees <= 10) {
     return "Wear a coat or a jacket and long pants";
   } else if (degrees > 10 && degrees < 20) {
     return "Wear a light jacket, especially if it's sunny";
