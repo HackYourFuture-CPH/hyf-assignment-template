59afb8d229e4e0c736d81b4661f7b9940b44a97e
hiwot <99hiwot@gmail.com>
2025-11-19 21:49:02 +0100

    When will we be there

---
+++ js-week3/travelinfo.js
@@ -0,0 +1,9 @@
+const travelInformation = {
+  speed: 90, //km/hr
+  destinationDistance: 400, //km
+};
+const travelTimeInHours = travelInformation.destinationDistance/travelInformation.speed;
+const hours= Math.floor(travelTimeInHours);
+const minutes= Math.round((travelTimeInHours-hours)*60);
+
+console.log("It will take to arrive: " + hours + " hours and " + minutes+ " minutes"); 
