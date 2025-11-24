166e7fb2d20b0ad892c1ca964f2702a2a08ccabf
hiwot <99hiwot@gmail.com>
2025-11-24 12:57:33 +0100

    DRY Applied

--- js-week3/smart-phone-usage-app.js
+++ js-week3/smart-phone-usage-app.js
@@ -7,15 +7,16 @@ activities.push({date:dateString, activity:activity, duration:duration});
 }
 // create showStatus function
 function showStatus(){
-     if(activities.length===0){
+    const activitiesLength=activities.length;
+     if(activitiesLength===0){
 console.log("Add some activities before calling showStatus");
 return;
      }
 let totalDuration=0;
-for (let i=0; i<activities.length;i++){
+for (let i=0; i<activitiesLength;i++){
     totalDuration += activities[i].duration;
 }
-    console.log("you have added "+ activities.length+" activities. They amount to "+totalDuration+" minutes of usage");
+    console.log("you have added "+ activitiesLength+" activities. They amount to "+totalDuration+" minutes of usage");
     console.log("Number of activities used are  "+ activities.length);
     if(totalDuration>usageLimit){
         console.log("You have reached your limit, no more smartphoning for you!");
@@ -23,17 +24,18 @@ for (let i=0; i<activities.length;i++){
 }
 //create logActivities function
 function logActivities(){
-    if(activities.length===0){
+    const activitiesLength=activities.length;
+    if(activitiesLength===0){
         console.log("No activities logged yet.");
         return;
     }
-    for(let i=0; i<activities.length;i++){
+    for(let i=0; i<activitiesLength;i++){
         const activity=activities[i];
         console.log("on "+ activity.date + " you used "+ activity.activity+" for "+ activity.duration+" minutes");
     }
 } 
 //add sample activity
-addActivity( "Facebook", 40);
+addActivity( "Facebook", 100);
 addActivity( "youtube", 30);
 addActivity( "telegram", 50);
     
