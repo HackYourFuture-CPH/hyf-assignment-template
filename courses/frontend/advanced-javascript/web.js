8aa962465f57a56a80959a91f63f052f61281367
Emebet <emymak93@yahoo.com>
2026-04-01 11:33:17 +0200

    feat:complete generateScreenshot() including  URL validation and RapidAPI request

--- courses/frontend/advanced-javascript/web.js
+++ courses/frontend/advanced-javascript/web.js
@@ -13,7 +13,7 @@ class ApiError extends Error {
 }
 
 class Screenshot {
-    constractor(id, url, imageUrl) {
+    constructor(id, url, imageUrl) {
         this.id = id;
         this.url = url;
         this.imageUrl = imageUrl;
@@ -31,11 +31,77 @@ class Screenshot {
 
 class App {
     constructor(){
-        this.previewImagUrl = null;
+        this.previewImageUrl = null;
     }
     async generateScreenshot() {
-        // TODO: implement API call
-       }
+        
+         try {
+        let url = document.getElementById("url-input").value.trim();
+
+        if (!url) {
+            throw new validationError("URL is required");
+        }
+        if (!url.startsWith("http://") && !url.startsWith("https://")) {
+            url = "https://" + url;
+        }
+
+
+
+        
+        const apiUrl = `https://website-screenshot6.p.rapidapi.com/screenshot?url=${encodeURIComponent(url)}&width=1920&height=1080`;
+
+        const options = {
+            method: "GET",
+            headers: {
+                "x-rapidapi-key": "67190f8610msha777cc237f812bep1cf694jsn955fa5181d17",
+                "x-rapidapi-host": "website-screenshot6.p.rapidapi.com",
+                "Content-Type": "application/json"
+            }
+        };
+
+        const response = await fetch(apiUrl, options);
+
+        if (!response.ok) {
+            throw new ApiError("Screenshot API failed");
+        }
+
+       const text = await response.text();
+       console.log("Raw API text:", text);
+
+       
+     const data = JSON.parse(text);
+     console.log("Parsed API response:", data);
+
+     const screenshotUrl = data.screenshotUrl;
+
+    if (!screenshotUrl) {
+    throw new ApiError("API did not return a screenshot URL");
+    }
+
+    // Show preview
+    document.getElementById("preview").src = screenshotUrl;
+
+    
+    this.previewImageUrl = screenshotUrl;
+
+     
+    document.getElementById("save-btn").disabled = false;
+
+       
+
+    } catch (err) {
+        if (err instanceof validationError) {
+            alert(err.toUserMessage());
+        } else if (err instanceof ApiError) {
+            alert(err.toUserMessage());
+        } else {
+            alert("Unexpected error while generating screenshot.");
+        }
+    }
+
+
+
+    }
 
        async saveScreenshot() {
            // TODO: implement save logic
