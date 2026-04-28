4f26ff9092d46f9c80a78c47e31ed55200443976
Anastasia Solodova <132252079+nastiasolo@users.noreply.github.com>
2026-04-28 09:03:44 +0200

    fetched rover pics and displayed them

--- courses/frontend/react/space-galactica-app/src/pages/NasaCollaborationPage/NasaCollaborationPage.jsx
+++ courses/frontend/react/space-galactica-app/src/pages/NasaCollaborationPage/NasaCollaborationPage.jsx
@@ -1,26 +1,28 @@
 import React, { useState, useEffect } from "react";
 import styles from "./NasaCollaborationPage.module.css";
+import { RoverPhoto } from "./RoverPhoto";
 
 const API_KEY = "u0jCIyZSv9nlupzHP3o2fJ9zKHe1ev8OrO8ouItH";
 
 const NASA_URLs = {
   astronomyPicOfTheDay: `https://api.nasa.gov/planetary/apod?api_key=${API_KEY}`,
-  marsRoverPhoto: `https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?earth_date=2015-6-3&api_key=${API_KEY}`,
+  marsRoverPhoto: `https://rovers.nebulum.one/api/v1/rovers/curiosity/photos?earth_date=2015-6-3`,
 };
 
 export const NasaCollaboration = () => {
   const [dailyImg, setDailyImg] = useState({});
   const [roverPhoto, setRoverPhoto] = useState({});
 
-  // useEffect(() => {
-  //   const fetchRoverPhotos = async () => {
-  //     const roverPhotoResponse = await fetch(NASA_URLs.marsRoverPhoto).then(
-  //       (response) => response.json(),
-  //     );
-  //     setRoverPhoto(roverPhotoResponse);
-  //   };
+  useEffect(() => {
+    const fetchRoverPhotos = async () => {
+      const roverPhotoResponse = await fetch(NASA_URLs.marsRoverPhoto).then(
+        (response) => response.json(),
+      );
+      setRoverPhoto(roverPhotoResponse);
+    };
 
-  //   fetchRoverPhotos();
+    fetchRoverPhotos();
+  }, []);
 
   useEffect(() => {
     const fetchDailyImg = async () => {
@@ -49,28 +51,22 @@ export const NasaCollaboration = () => {
         </section>
         <section className="card">
           <h2>Rover Photos</h2>
-          {/* 🧑🏽‍🚀 Task - Week 3 */}
-          {/* Iterate over the roverPhoto?.photos array and display all the pictures. */}
-          {roverPhoto?.photos?.length ? (
-            <>
-              {/* 🧑🏽‍🚀 Task - Week 3 */}
-              {/* Create a react component for the <RoverPhoto />, which should accept the following props: */}
-              {/* 1. src: source of the img; */}
-              {/* 2. date: earth_date data coming from the API; */}
-              {/* 3. roverName: will be in the rover object. */}
-
-              {/* If you don't know how the data looks like you can log it out to the console and investigate in the browser's devtools. */}
-
-              <p>Date {roverPhoto.photos[0]?.earth_date}</p>
-              <img
-                className={styles.nasaPicOfTheDayImg}
-                src={roverPhoto.photos[0]?.img_src}
-                alt={dailyImg.title}
-              />
-            </>
-          ) : (
-            <p>Loading rover photos...</p>
-          )}
+          <div className={styles.roverContainer}>
+            {/* 🧑🏽‍🚀 Task - Week 3 */}
+            {/* Iterate over the roverPhoto?.photos array and display all the pictures. */}
+            {roverPhoto?.photos?.length > 0 ? (
+              roverPhoto.photos.map((photo) => (
+                <RoverPhoto
+                  key={photo.id}
+                  src={photo.img_src}
+                  date={photo.earth_date}
+                  roverName={photo.rover.name}
+                />
+              ))
+            ) : (
+              <p>Loading rover photos...</p>
+            )}
+          </div>
         </section>
       </main>
     </div>
